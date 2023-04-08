bd5a13cc87be61df37fddb3b4926a310bad3881d
Raven <raven.xu98@gmail.com>
2023-04-09 03:58:49 +0800

    import unified data structure

--- examples/other/communicate/communicate.h
+++ examples/other/communicate/communicate.h
@@ -1,15 +1,26 @@
-//
-// Created by 紫意菌 on 4/2/23.
-//
-
+// Created by Ziyi on 4/2/23.
 #ifndef __COMMUNICATE_H__
 #define __COMMUNICATE_H__
-void CPXListeningInit(void);
-void itoa(uint8_t number,char*numberArray);
+
+#define MAPPING_REQ 1
+#define EXPLORE_REQ 2
+#define PATH_REQ 3
+#define EXPLORE_RESP 4
+#define PATH_RESP 5
+
 typedef struct
 {
     uint16_t x;
     uint16_t y;
     uint16_t z;
 } coordinate_t;
+
+typedef struct
+{
+    coordinate_t startPoint;
+    coordinate_t endPoint;
+} coordinate_pair_t;
+
+void CPXListeningInit(void);
+void itoa(uint8_t number,char*numberArray);
 #endif //__COMMUNICATE_H__
